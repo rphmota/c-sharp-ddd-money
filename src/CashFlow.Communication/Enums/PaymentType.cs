9761eedff693b52e8b024ba1b2b4b547c0706af9
rphmota <rphmota@gmail.com>
2025-07-21 22:57:08 -0300

    WIP adicionando basico de expense post

---
+++ src/CashFlow.Communication/Enums/PaymentType.cs
@@ -0,0 +1,14 @@
+﻿namespace CashFlow.Communication.Enums
+{
+    public enum PaymentType
+    {
+        Cash = 1,
+        CreditCard = 2,
+        DebitCard = 3,
+        BankTransfer = 4,
+        Check = 5,
+        MobilePayment = 6,
+        Cryptocurrency = 7,
+        Other = 8
+    }
+}
